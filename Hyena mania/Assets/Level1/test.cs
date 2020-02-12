8ab4de675441560647a5c928d5eae97b0d21d852
KO-ray <KO-ray@users.noreply.github.com>
2020-02-12 10:38:34 +0100

    test print

--- Hyena mania/Assets/Level1/test.cs
+++ Hyena mania/Assets/Level1/test.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public bool bruh;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("stinky haha");
     }
 
     // Update is called once per frame

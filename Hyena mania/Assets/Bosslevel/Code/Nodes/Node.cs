f3cc83b6caa96b69c20dbccbbb8a4d5326ff08a9
ArtF4rt <60962761+ArtF4rt@users.noreply.github.com>
2020-03-16 20:43:28 +0100

    Nodes + TowerBuilding

---
+++ Hyena mania/Assets/Bosslevel/Code/Nodes/Node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public GameObject spawn;
+    private GameObject tower;
+    public Vector3 positionOffset;
+    
+    public void OnMouseDown()
+    {
+        //if there's already a turret, you can't buil here
+        if (tower != null)
+        {
+            Debug.Log("Tower already build--On screen");
+            return;
+        }
+        
+        //Build tower
+        GameObject towerToBuild = BuildManager.instance.GetTowerToBuild();
+        tower = (GameObject)Instantiate(towerToBuild, spawn.transform.position + positionOffset, spawn.transform.rotation);
+    }
+
+    public void OnMouseEnter()
+    {
+        //Switches matrial emission on 
+        gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+    }
+
+    public void OnMouseExit()
+    {
+        //Switches material emission off
+        gameObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+    }
+}
---
+++ Hyena mania/Assets/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{//Start singleton
+    public static BuildManager instance;//Stores the Buildmanager in a reference
+
+     void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one Buildmanager in scene!");
+            return;
+        }
+        instance = this; //referring to this script (There can now only be one Buildmanager)
+
+    }
+//End singleton
+
+    private GameObject towerToBuild;
+
+    public GameObject standardTowerPrefab;
+
+    void Start()
+    {
+        towerToBuild = standardTowerPrefab;
+
+    }
+    public GameObject GetTowerToBuild()
+    {
+        return towerToBuild;
+
+    }
+}

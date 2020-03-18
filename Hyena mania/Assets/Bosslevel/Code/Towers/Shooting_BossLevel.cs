54d1135092932d7485c8ed8a31fe383a9da193ca
ArtF4rt <60962761+ArtF4rt@users.noreply.github.com>
2020-03-18 21:43:24 +0100

    Coding Towers

---
+++ Hyena mania/Assets/Bosslevel/Code/Towers/Shooting_BossLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting_BossLevel : MonoBehaviour
+{
+    public GameObject enemy;
+    public float range;
+    private float distanceToEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       enemy = GameObject.Find("Enemy");
+       distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(enemy.transform);
+
+        if (distanceToEnemy <= range)
+        {
+            Debug.Log(distanceToEnemy);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Hyena mania/Assets/Level1/Code/Shooting.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform target;
+    public float range;
+
+    public float startCheck;
+    public float updateCheck;
+
+    public string enemyTag = "Enemy";
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", startCheck, updateCheck);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+            
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}

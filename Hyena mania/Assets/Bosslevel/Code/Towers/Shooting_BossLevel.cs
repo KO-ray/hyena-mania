4bfaf55bf03e7b9acadc2b0426e2a18f91856647
ArtF4rt <60962761+ArtF4rt@users.noreply.github.com>
2020-03-18 22:01:38 +0100

    Error fixes

--- Hyena mania/Assets/Bosslevel/Code/Towers/Shooting_BossLevel.cs
+++ Hyena mania/Assets/Bosslevel/Code/Towers/Shooting_BossLevel.cs
@@ -11,24 +11,40 @@ public class Shooting_BossLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       enemy = GameObject.Find("Enemy");
-       distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+       enemy = GameObject.FindGameObjectWithTag("Enemy");
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
         transform.LookAt(enemy.transform);
 
         if (distanceToEnemy <= range)
         {
-            Debug.Log(distanceToEnemy);
+            ShootRock();
         }
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    void ShootRock()
+    {
+        //instantiate rock
+        //look at enemy
+        //translate to shoot object
+        //MAKE ROCK TRIGGER
+        
+    }
+   void OnTriggerEnter(Collider other)
+    {
+        //check bool
+        //DoDamage
     }
 }
